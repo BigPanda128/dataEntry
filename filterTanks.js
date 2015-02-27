b75840815d861086f725ccb267612392ab196dc6
Ryan Postma <rpostma@sjccs142.saintjoe.edu>
2015-02-27 00:26:31 -0500

    commented version

--- filterTanks.js
+++ filterTanks.js
@@ -3,11 +3,13 @@
 //2/20/2015
 //filter for tanks
 
-
+//call on the main tank class
 load('tanks.js');
 
+//divide the data into a single variable
 var tanks = JSON.parse(TANKS_FILE);
 
+//filter funtion that brings back data from the list that the user is looking for
 function filter(array, test) {
 	var passed = [];
 	for (var i = 0; i < array.length; i++) {
@@ -18,6 +20,7 @@ function filter(array, test) {
 	return passed;
 }
 
+//where i determine what i actually an filtering for. in ths case its tanks with a top speed over 70 km/h
 print(JSON.stringify(filter(tanks, function(tank) {
 	return tank.speed > 70;
 })));
